ced7db373d6098120e17ff9c6ac462030509849d
Abhishek Srivastava <58135974+NoviceMAn-prog@users.noreply.github.com>
2022-01-12 01:49:58 +0530

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,109 @@
+import os
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import os
+from glob import glob
+from PIL import Image
+np.random.seed(123)
+import warnings
+import numpy as np
+from keras import backend as K
+from keras.optimizers import Adam, Nadam
+import tensorflow as tf
+def el(y_true, y_pred):
+    l = keras.losses.BinaryCrossentropy(y_true,y_pred)
+    return l
+def get_optimizer():
+    
+    adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    return adam
+
+def single_dice_coef(y_true, y_pred_bin):
+    # shape of y_true and y_pred_bin: (height, width)
+    intersection = np.sum(y_true * y_pred_bin)
+    if (np.sum(y_true)==0) and (np.sum(y_pred_bin)==0):
+        return 1
+    return (2*intersection) / (np.sum(y_true) + np.sum(y_pred_bin))
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def mean_dice_coef(y_true, y_pred_bin):
+    # shape of y_true and y_pred_bin: (n_samples, height, width, n_channels)
+    batch_size = y_true.shape[0]
+    channel_num = y_true.shape[-1]
+    mean_dice_channel = 0.
+    for i in range(batch_size):
+        for j in range(channel_num):
+            channel_dice = single_dice_coef(y_true[i, :, :, j], y_pred_bin[i, :, :, j])
+            mean_dice_channel += channel_dice/(channel_num*batch_size)
+    return mean_dice_channel
+
+def seg_loss(y_true, y_pred):
+    dice_s = dice_coefficient_loss(y_true,y_pred)
+    
+    #ce_loss = BinaryCrossentropy(y_true,y_pred)
+    ce_loss =tf.keras.backend.binary_crossentropy(y_true,y_pred)
+    
+    return ce_loss +dice_s
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def dice_coefficient_loss(y_true, y_pred):
+    return 1.-dice_coefficient(y_true, y_pred)
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+    loss = weight * (logit_y_pred * (1. - y_true) +
+                     K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+def weighted_dice(y_true, y_pred):
+    smooth = 1.
+    w, m1, m2 = 0.7, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    return K.sum(score)
+
+def weighted_dice_loss(y_true, y_pred):
+    smooth = 1.
+    w, m1, m2 = 0.7, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    loss = 1. - K.sum(score)
+    return loss
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd
+    averaged_mask = K.pool2d(y_true, pool_size=(50, 50), strides=(1, 1), padding='same', pool_mode='avg')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight = 5. * K.exp(-5. * K.abs(averaged_mask - 0.5))
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + dice_loss(y_true, y_pred)
+    return loss
+def dice_coefficient(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true[:,:,:,0])
+    y_pred_f = K.flatten(y_pred[:,:,:,0])
+    intersection = K.sum(y_true_f * y_pred_f)
+    d1 =  (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return d1
---
+++ utils.py
@@ -0,0 +1,71 @@
+import os
+from PIL import Image
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import os
+from glob import glob
+np.random.seed(123)
+import warnings
+import numpy as np
+from sklearn.utils import shuffle
+import cv2
+
+
+def create_dir(path):
+    """ Create a directory. """
+    try:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    except OSError:
+        print(f"Error: creating directory with name {path}")
+
+def read_data(x, y):
+    """ Read the image and mask from the given path. """
+    image = cv2.imread(x, cv2.IMREAD_COLOR)
+    mask = cv2.imread(y, cv2.IMREAD_COLOR)
+    return image, mask
+
+def read_params():
+    """ Reading the parameters from the JSON file."""
+    with open("params.json", "r") as f:
+        data = f.read()
+        params = json.loads(data)
+        return params
+
+def load_data(path):
+    """ Loading the data from the given path. """
+    images_path = os.path.join(path, "image/*")
+    masks_path  = os.path.join(path, "mask/*")
+    
+    images = glob(images_path)
+    masks  = glob(masks_path)
+    
+    return images, masks
+
+def shuffling(x, y):
+    x, y = shuffle(x, y, random_state=42)
+    return x, y
+def get_image(image_path, image_size_wight, image_size_height,gray=False):
+    # load image
+    img = Image.open(image_path)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    
+    if gray==True:
+        img = img.convert('L')
+    # center crop
+    img_center_crop = img
+    # resize
+    img_resized = img_center_crop.resize((image_size_height, image_size_wight), Image.ANTIALIAS)
+    edge = cv2.Canny(np.asarray(np.uint8(img_resized)),10,1000)
+    
+    flag = False
+    # convert to numpy and normalize
+    img_array = np.asarray(img_resized).astype(np.float32)/255.0
+    edge = np.asarray(edge).astype(np.float32)/255.0
+    #print(img_array)
+    if gray==True:
+        img_array=(img_array >=0.5).astype(int)
+    img.close()
+    return img_array,edge
